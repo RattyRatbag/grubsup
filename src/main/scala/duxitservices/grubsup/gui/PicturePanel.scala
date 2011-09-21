45fd148d4bf51861c2a7303de016daac8a25d73e
David Edmonds <edmonds.d.r@gmail.com>
2011-09-21 16:56:50 +0100

    - Displayed thumbnail of image in picture panel

--- src/main/scala/duxitservices/grubsup/gui/PicturePanel.scala
+++ src/main/scala/duxitservices/grubsup/gui/PicturePanel.scala
@@ -3,15 +3,29 @@ package duxitservices.grubsup.gui
 import swing._
 import swing.BorderPanel.Position
 import duxitservices.grubsup.entities.Picture
-import javax.swing.ImageIcon
 import collection.mutable.ArrayBuffer
+import javax.swing.{JLabel, ImageIcon}
+import javax.imageio.ImageIO
+import java.io.File
+import java.awt.{Image, Color}
 
 /**
  * @author David Edmonds <edmonds.d.r@gmail.com>
  */
 
 class PicturePanel extends BorderPanel {
-  val pictureListView = new ListView[Picture]
+  val pictureListView = new ListView[Picture] {
+    renderer = new ListView.Renderer[Picture] {
+      def componentFor(list: ListView[_], isSelected: Boolean, focused: Boolean, a: Picture, index: Int) = {
+        val label = ListView.GenericRenderer.componentFor(list, isSelected, focused, a, index)
+        val jLabel = label.peer.asInstanceOf[JLabel]
+        jLabel.setText(index.toString)
+        val image = ImageIO.read(new File(a.fileName))
+        jLabel.setIcon(new ImageIcon(image.getScaledInstance(200, 150, Image.SCALE_SMOOTH)))
+        label
+      }
+    }
+  }
   var pictureList = new ArrayBuffer[Picture]
   val chooser = new FileChooser
 
